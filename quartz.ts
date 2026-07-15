65260966536dd2f17ac770dfceb64fcc2ea81656
segara-alam <abimanyfaishol@gmail.com>
2026-07-15 19:52:16 +0300

    Quartz sync: Jul 15, 2026, 7:52 PM

--- quartz.ts
+++ quartz.ts
@@ -1,7 +1,5 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
 
-const config = await loadQuartzConfig({
-  Plugin.ClickableImages(),
-})
+const config = await loadQuartzConfig()
 export default config
 export const layout = await loadQuartzLayout()
